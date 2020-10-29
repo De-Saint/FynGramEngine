46758c6b3a6aa265ffee1e62d898ff02af29f45a
SaintDeemene <work@baesicsolutions.com>
2020-10-29 12:17:31 +0100

    adding backend connection

--- src/java/fgengine/Mobile/MUserServlet.java
+++ src/java/fgengine/Mobile/MUserServlet.java
@@ -3,14 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.Mobile.Services;
+package fgengine.Mobile;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import fgengine.Managers.EngineUserManager;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -31,16 +40,6 @@ public class MUserServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet MUserServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet MUserServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
         }
     }
 
@@ -70,7 +69,89 @@ public class MUserServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            String ans = "";
+            String json = "";
+            StringBuilder sb = new StringBuilder();
+            try {
+                BufferedReader br = request.getReader();
+                String str = null;
+                while ((str = br.readLine()) != null) {
+                    sb.append(str);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            JSONParser parser = new JSONParser();
+            JSONObject jsonParameter = null;
+            try {
+                jsonParameter = (JSONObject) parser.parse(sb.toString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            String type = (String) jsonParameter.get("type");
+            String json1 = "";
+            String json2 = "";
+            String json3 = "";
+            String result = "";
+            switch (type) {
+                case "Login": {
+                    String Email_PhoneNumber = (String) jsonParameter.get("emailphone");
+                    String Password = (String) jsonParameter.get("password");
+                    int UserID = 0;
+                    String OldSessionID = (String) jsonParameter.get("oldsession");
+                    HttpSession session = request.getSession(true);
+                    JsonObject returninfo = new JsonObject();
+                    if (EngineUserManager.checkEmailAddressOrPhoneNumberExist(Email_PhoneNumber)) {
+                        UserID = EngineUserManager.checkPasswordEmailMatch(Password, Email_PhoneNumber);
+                        if (UserID != 0) {
+                            String NewSessionID = "";
+                            int usertypeid = EngineUserManager.GetUserTypeIDByUserID("" + UserID);
+                            String usertype = "";
+                            switch (usertypeid) {
+                                case 1:
+                                    NewSessionID = session.getId() + "#A";
+                                    usertype = "Admin";
+                                    session.invalidate();
+                                    break;
+                                case 2:
+                                    NewSessionID = session.getId() + "#S";
+                                    usertype = "Seller";
+                                    session.invalidate();
+                                    break;
+                                case 3:
+                                    NewSessionID = session.getId() + "#C";
+                                    usertype = "Customer";
+                                    session.invalidate();
+                                    break;
+                                default:
+                                    break;
+                            }
+                            EngineUserManager.UpdateUserSessionDetails(OldSessionID, NewSessionID, "" + UserID, "FynGramShop");
+                            JsonObject dataobject = new JsonObject();
+                            dataobject.addProperty("sessionid", NewSessionID);
+                            dataobject.addProperty("sessiontype", usertype);
+                            returninfo.add("data", dataobject);
+                            returninfo.addProperty("status", "success");
+                            returninfo.addProperty("msg", "Successful Login");
+                        } else {
+                            returninfo.addProperty("status", "error");
+                            returninfo.addProperty("msg", "Incorrect Login Details.");
+                        }
+                    } else {
+                        returninfo.addProperty("status", "error");
+                        returninfo.addProperty("msg", "Email or Phone Number Entered Doesn't Exist.");
+                    }
+                    json = new Gson().toJson((JsonElement) returninfo);
+                }
+            }
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(json);
+        } catch (Exception ex) {
+
+        }
     }
 
     /**
